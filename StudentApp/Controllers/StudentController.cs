d66e0ae44bb2ca891d255a0a0ce0e101b6029ab5
HASSAN ALI <HASSAN ALI@DESKTOP-D5S3FO5>
2023-08-30 20:58:16 -0700

    Add project files.

---
+++ StudentApp/Controllers/StudentController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentApp.Models;
+
+namespace StudentApp.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        public ViewResult Remove(int id)
+        {
+            Student s = StudentRepository.students.Find(s => s.Id == id);
+            StudentRepository.students.Remove(s);
+            return View("ListStudent", StudentRepository.students);
+        }
+        public ViewResult Details(int id)
+        {
+            Student s=StudentRepository.students.Find(s=>s.Id==id); 
+            return View("Thanks",s);
+        }
+
+        public ViewResult Edit(int id)
+        {
+            Student s = StudentRepository.students.Find(s => s.Id == id);
+            return View("Edit", s);
+        }
+        [HttpPost]
+        public ViewResult Edit(Student s)
+        {
+            foreach(Student std in StudentRepository.students)
+            {
+                if(std.Id==s.Id)
+                {
+                    std.Name = s.Name;
+                    std.CGPA = s.CGPA;
+                    std.Semester = s.Semester;
+                    break;
+                }
+            }
+            return View("ListStudent", StudentRepository.students);
+        }
+        public ViewResult Index()
+
+        {
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ViewResult StudentForm() 
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ViewResult StudentForm(Student s)
+        {
+            StudentRepository.AddStudent(s);
+            
+            return View("Thanks",s);
+        }
+
+        public ViewResult ListStudent()
+        {
+            return View(StudentRepository.students);
+        }
+        //public ViewResult StudentForm(int id, string name,float CGPA,string semester)
+        //{
+        //    return View();
+        //}
+    }
+}
---
+++ StudentApp/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace StudentApp.Models;
+using System.Threading;
+
+public class Student
+{
+    [Required(ErrorMessage="Please Enter Name")]
+    public string Name { get; set; }
+
+    //public int age { get; set; }
+    [Required(ErrorMessage ="Please Enter Id")]
+
+
+    public static int _currentId = 0;
+    public int Id { get;}=Interlocked.Increment(ref _currentId);
+        
+    //public string email { get; set; }
+
+    public float CGPA { get; set; }
+    [Required(ErrorMessage = "Please Enter Id")]
+    [Range(1,10)]
+    public string Semester { get; set; }
+
+}
